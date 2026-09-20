3b698f20847315a843ee5df99a955e361db5d553
Bhavya Vaishnav <vaishnavbhavya1701@gmail.com>
2026-09-20 16:03:34 +0530

    feat: add swagger along with openAPI to create API documentation

--- src/main/java/com/bhavya/skillswap/config/SecurityConfig.java
+++ src/main/java/com/bhavya/skillswap/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
